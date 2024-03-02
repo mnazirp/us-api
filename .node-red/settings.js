cc10b30f5fdcf9d057a687b88e3f351a78265953
mnazirp <mnazirp@gmail.com>
2024-03-02 23:28:15 +0700

    init

---
+++ .node-red/settings.js
@@ -0,0 +1,81 @@
+const envy = require('@envyjs/node');
+envy.enableTracing({
+  serviceName: process.env.SERVICE_NAME,
+});
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+module.exports = {
+  flowFile: "flows.json",
+  flowFilePretty: true,
+  httpAdminRoot: "/admin",
+  httpAdminMiddleware: function (req, res, next) {
+    res.set("X-Frame-Options", "sameorigin");
+    next();
+  },
+  credentialSecret: false,
+  // adminAuth: require("node-red-auth-github")({
+  //   clientID: process.env.NODERED_CLIENT_ID || "empty-id",
+  //   clientSecret: process.env.NODERED_CLIENT_SECRET || "empty-secret",
+  //   baseURL: process.env.NODERED_BASE_URL,
+  //   users: [
+  //     { username: "mnazirp", permissions: ["*"] },
+  //   ],
+  // }),
+  httpNodeCors: {
+    origin: "*",
+    methods: "GET,PUT,POST,DELETE"
+  },
+  adminAuth: {
+    type: "credentials",
+    users: [
+      {
+        username: "admin",
+        password: "$2b$08$b4fq3gNtqk0aTMx.2oCN3u3duMc/OxlJJRMfMsuPY/00GgzaJ63wW",
+        permissions: "*"
+      },
+      {
+        username: "operator",
+        password: "$2b$08$b4fq3gNtqk0aTMx.2oCN3u3duMc/OxlJJRMfMsuPY/00GgzaJ63wW",
+        permissions: "read"
+      }
+    ]
+  },
+  contextStorage: {
+    default: "memoryOnly",
+    memoryOnly: { module: 'memory' },
+    file: { module: 'localfilesystem' }
+  },
+  uiPort: process.env.NODEREDPORT || 1880,
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false
+    }
+  },
+  exportGlobalContextKeys: false,
+  externalModules: {
+  },
+  editorTheme: {
+    theme: "midnight-red",
+    palette: {},
+    projects: {
+      enabled: true,
+      workflow: {
+        mode: "manual"
+      }
+    },
+    codeEditor: {
+      lib: "monaco",
+      options: {
+        theme: "vs",
+      }
+    }
+  },
+  functionExternalModules: true,
+  functionGlobalContext: {
+    NODE_TLS_REJECT_UNAUTHORIZED: 0,
+  },
+  debugMaxLength: 1000,
+  mqttReconnectTime: 15000,
+  serialReconnectTime: 15000,
+}
