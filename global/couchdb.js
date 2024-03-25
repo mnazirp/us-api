384135834a89391a9fbe9f705ec028d597cc0a41
mnazirp <mnazirp@gmail.com>
2024-03-25 16:07:12 +0700

    https

--- global/couchdb.js
+++ global/couchdb.js
@@ -1,112 +1,90 @@
 require('dotenv').config();
 const nano = require('nano');
 
+const isHttps = process.env.DOCS_MASTER.includes('https');
+/** /extra here for purpose nano can get sub-domain not just domain */
+let fullUrl = (isHttps) ? process.env.DOCS_MASTER + '/extra' : process.env.DOCS_MASTER;
 const couch = nano({
-  url: process.env.DOCS_MASTER_URL,
+  url: fullUrl,
   requestDefaults: {
     rejeceUnauthorized: false
   }
 });
-const context = 'couchdb'
+const contex = 'couchdb';
 
 async function openByID(database, _id) {
   try {
-    const db = couch.use(database);
+    const db = (isHttps) ? couch.server.use(database) : couch.use(database);
     let data = await db.get(_id);
     return {
       success: true,
-      context,
+      context: contex,
       func: 'openByID',
       data
     }
   } catch (err) {
-    throw {
-      success: false,
-      context,
-      func: 'commands',
-      message: err.toString()
-    };
+    throw new Error(err.message);
   }
 }
 
 async function view(database, design, view, params) {
   try {
-    const db = couch.use(database);
+    const db = (isHttps) ? couch.server.use(database) : couch.use(database);
     let data = await db.view(design, view, params);
     return {
       success: true,
-      context,
+      context: contex,
       func: 'view',
       meta: { database, design, view, params },
       data
     }
   } catch (err) {
-    throw {
-      success: false,
-      context,
-      func: 'commands',
-      message: err.toString()
-    };
+    throw new Error(err.message);
   }
 }
 
 async function insert(database, document) {
   try {
-    const db = couch.use(database);
+    const db = (isHttps) ? couch.server.use(database) : couch.use(database);
     let rs = await db.insert(document);
     return {
       success: true,
-      context,
+      context: contex,
       func: 'insert',
       data: rs
     };
   } catch (err) {
-    throw {
-      success: false,
-      context,
-      func: 'commands',
-      message: err.toString()
-    };
+    throw new Error(err.message);
   }
 }
 
 async function bulk(database, documents) {
   try {
-    const db = couch.use(database);
+    const db = (isHttps) ? couch.server.use(database) : couch.use(database);
     let rs = await db.bulk({ docs: documents });
     return {
       success: true,
-      context,
+      context: contex,
       func: 'bulk',
       data: rs
     };
   } catch (err) {
-    throw {
-      success: false,
-      context,
-      func: 'commands',
-      message: err.toString()
-    };
+    throw new Error(err.message);
   }
 }
 
 async function list(database) {
   try {
-    const db = couch.use(database);
+    const db = (isHttps) ? couch.server.use(database) : couch.use(database);
     let data = await db.list({ include_docs: true });
     return {
       success: true,
-      context,
+      context: contex,
       func: 'list',
       data
     }
   } catch (err) {
-    throw {
-      success: false,
-      context,
-      func: 'commands',
-      message: err.toString()
-    };
+    throw new Error(err.message);
   }
 }
 
